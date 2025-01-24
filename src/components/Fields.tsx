c20ce12a70c29fde981e49a3eae407060ac8f48d
Logan Kimball <logankimbs@gmail.com>
2025-01-24 12:59:41 -0800

    Added contact-us page

--- src/components/Fields.tsx
+++ src/components/Fields.tsx
@@ -4,11 +4,22 @@ import clsx from 'clsx';
 const formClasses =
   'block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 focus:border-wo-blue focus:bg-white focus:outline-none focus:ring-wo-blue sm:text-sm';
 
-function Label({ id, children }: { id: string; children: React.ReactNode }) {
+function Label({
+  id,
+  children,
+  className,
+}: {
+  id: string;
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
     <label
       htmlFor={id}
-      className="mb-3 block text-sm font-medium text-gray-700"
+      className={clsx(
+        'mb-3 block text-sm font-medium text-gray-700',
+        className,
+      )}
     >
       {children}
     </label>
@@ -17,24 +28,33 @@ function Label({ id, children }: { id: string; children: React.ReactNode }) {
 
 export function TextArea({
   label,
+  description,
   rows = 4,
   className,
   ...props
-}: Omit<React.ComponentPropsWithoutRef<'textarea'>, 'id'> & { label: string }) {
+}: Omit<React.ComponentPropsWithoutRef<'textarea'>, 'id'> & {
+  label: string;
+  description?: string;
+}) {
   const id = useId();
+  const descriptionId = `${id}-description`;
 
   return (
     <div className={className}>
-      {label && <Label id={id}>{label}</Label>}
+      <div className="flex justify-between">
+        {label && <Label id={id}>{label}</Label>}
+        {description && (
+          <Label id={descriptionId} className="font-normal text-gray-400">
+            {description}
+          </Label>
+        )}
+      </div>
       <textarea
         id={id}
         rows={rows}
+        aria-describedby={description ? descriptionId : undefined}
+        className={clsx(formClasses, 'resize-none', className)}
         {...props}
-        className={clsx(
-          formClasses,
-          'resize-none', // Prevent resizing unless specified
-          className,
-        )}
       />
     </div>
   );
@@ -42,15 +62,27 @@ export function TextArea({
 
 export function TextField({
   label,
+  description,
   type = 'text',
   className,
   ...props
-}: Omit<React.ComponentPropsWithoutRef<'input'>, 'id'> & { label: string }) {
+}: Omit<React.ComponentPropsWithoutRef<'input'>, 'id'> & {
+  label: string;
+  description?: string;
+}) {
   let id = useId();
+  const descriptionId = `${id}-description`;
 
   return (
     <div className={className}>
-      {label && <Label id={id}>{label}</Label>}
+      <div className="flex justify-between">
+        {label && <Label id={id}>{label}</Label>}
+        {description && (
+          <Label id={descriptionId} className="font-normal text-gray-400">
+            {description}
+          </Label>
+        )}
+      </div>
       <input id={id} type={type} {...props} className={formClasses} />
     </div>
   );
