60384990a95039f935396f87fe0a80d49fe3ead8
Peter Kimball <peterkimball@Peters-MacBook-Pro-14in.local>
2025-01-28 14:38:27 -0800

    Updated header in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,7 +17,7 @@ export function Hero() {
       <div className="relative">
         <Container className="pb-16 pt-20 text-center lg:pt-32">
           <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-            A new{' '}
+            A old{' '}
             <span className="relative whitespace-nowrap text-wo-blue">
               <span className="relative">swell</span>
             </span>{' '}
