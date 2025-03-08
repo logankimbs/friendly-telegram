5abe9f1dd3e6e644cf3d3a1e3f95b72f537375e2
Logan Kimball <logankimbs@gmail.com>
2025-03-08 12:46:13 -0800

    Update container

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,25 +3,26 @@ import Link from 'next/link';
 import { Button } from '@/components/Button';
 import { Logo } from '@/components/Logo';
 import { SlimLayout } from '@/components/SlimLayout';
+import { Container } from '@/components/Container';
+import React from 'react';
 
 export default function NotFound() {
   return (
-    <SlimLayout>
-      <div className="flex">
-        <Link href="/" aria-label="Home">
-          <Logo className="h-10 w-auto" />
-        </Link>
-      </div>
-      <p className="mt-20 text-sm font-medium text-gray-700">404</p>
-      <h1 className="mt-3 text-lg font-semibold text-gray-900">
-        Page not found
-      </h1>
-      <p className="mt-3 text-sm text-gray-700">
-        Sorry, we couldn’t find the page you’re looking for.
-      </p>
-      <Button href="/" className="mt-10">
-        Go back home
-      </Button>
-    </SlimLayout>
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <Container className="text-center">
+        <p className="text-base font-semibold text-wo-blue">404</p>
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+          Page not found
+        </h1>
+        <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Button href="/" color="blue">
+            Go back home
+          </Button>
+        </div>
+      </Container>
+    </main>
   );
 }
