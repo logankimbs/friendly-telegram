c148c3cb1dc0f7d863d0b5825ab2799e449408c0
Logan Kimball <logankimbs@gmail.com>
2025-03-09 07:07:13 -0700

    Add social intents

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Script from 'next/script';
+import '@/styles/tailwind.css';
+import { rootMetadata } from '@/utils/metadata';
 import clsx from 'clsx';
 import { type Metadata } from 'next';
 import { Inter, Lexend } from 'next/font/google';
-import { rootMetadata } from '@/utils/metadata';
-import '@/styles/tailwind.css';
+import Script from 'next/script';
+import React from 'react';
 
 export const metadata: Metadata = rootMetadata;
 
@@ -49,6 +49,13 @@ export default function RootLayout({
           gtag('config', 'UA-134344710-1');
         `}
       </Script>
+
+      {/* Social Intents */}
+      <Script
+        src="https://www.socialintents.com/api/socialintents.1.3.js#2c9fa5636221129d01622c450abf06ee"
+        type="text/javascript"
+        strategy="afterInteractive"
+      />
     </html>
   );
 }
