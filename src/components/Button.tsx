80437659a95ff96032547015b2f80f8c699bfd4c
Logan Kimball <logankimbs@gmail.com>
2025-03-10 10:57:53 -0700

    Add disabled state

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const baseStyles = {
   solid:
-    'group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2',
+    'group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 disabled:bg-gray-100 disabled:text-gray-400',
   outline:
     'group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none',
 };
