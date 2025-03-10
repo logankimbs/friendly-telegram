f80dad0163680005508d971858cc272f7c40a09e
Logan Kimball <logankimbs@gmail.com>
2025-03-10 10:58:12 -0700

    Add disabled state

--- src/components/Fields.tsx
+++ src/components/Fields.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import React, { useId } from 'react';
 
 const formClasses =
-  'block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 focus:border-wo-blue focus:bg-white focus:outline-none focus:ring-wo-blue sm:text-sm';
+  'block w-full appearance-none rounded-md border border-gray-200 bg-gray-50 px-3 py-2 text-gray-900 placeholder-gray-400 focus:border-wo-blue focus:bg-white focus:outline-none focus:ring-wo-blue sm:text-sm disabled:bg-gray-100 disabled:text-gray-400';
 
 function Label({
   id,
