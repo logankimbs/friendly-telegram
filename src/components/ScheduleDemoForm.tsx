4d1e4214c477e7b8dfe8ec273f0693cbb2de6571
Logan Kimball <logankimbs@gmail.com>
2025-03-19 12:55:09 -0700

    Update form to work with react-hook-form and zod

--- src/components/ScheduleDemoForm.tsx
+++ src/components/ScheduleDemoForm.tsx
@@ -4,7 +4,10 @@ import { Alert, AlertProps } from '@/components/Alert';
 import { Button } from '@/components/Button';
 import { TextField } from '@/components/Fields';
 import { submitScheduleDemoForm } from '@/utils/api';
-import { FormEvent, useState } from 'react';
+import { scheduleDemoFormSchema } from '@/utils/schemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 const SUCCESS_ALERT = {
   status: 'success',
@@ -19,27 +22,30 @@ const ERROR_ALERT = {
   description: 'There was an error processing your request. Please try again.',
 } satisfies AlertProps;
 
-export function DemoForm() {
+export type ScheduleDemoFormFields = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  phone?: string;
+};
+
+export function ScheduleDemoForm() {
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
   const [alert, setAlert] = useState<AlertProps | null>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ScheduleDemoFormFields>({
+    mode: 'onTouched',
+    resolver: zodResolver(scheduleDemoFormSchema),
+  });
 
-  const handleSubmitForm = async (
-    event: FormEvent<HTMLFormElement>,
-  ): Promise<void> => {
-    event.preventDefault();
-
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData.entries());
-
-    // todo: validate data
-
+  const onSubmit: SubmitHandler<ScheduleDemoFormFields> = async (data) => {
     try {
       setIsSubmitting(true);
       await submitScheduleDemoForm(data);
       setAlert(SUCCESS_ALERT);
-
-      form.reset();
     } catch (error) {
       setIsSubmitting(false);
       setAlert(ERROR_ALERT);
@@ -50,54 +56,55 @@ export function DemoForm() {
     <div>
       {alert && <Alert {...alert} className="mt-10" />}
       <form
-        onSubmit={handleSubmitForm}
+        onSubmit={handleSubmit(onSubmit)}
         className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-2"
       >
         <TextField
           className="col-span-full"
           label="Email address"
-          name="email"
           type="email"
           autoComplete="email"
           disabled={isSubmitting}
+          error={errors.email?.message}
+          {...register('email')}
           required
         />
         <TextField
           label="First name"
-          name="first-name"
           type="text"
           autoComplete="given-name"
           disabled={isSubmitting}
+          error={errors.firstName?.message}
+          {...register('firstName')}
           required
         />
         <TextField
           label="Last name"
-          name="last-name"
           type="text"
           autoComplete="family-name"
           disabled={isSubmitting}
+          error={errors.lastName?.message}
+          {...register('lastName')}
           required
         />
         <TextField
           className="col-span-full"
           label="Phone number"
-          description="Optional"
-          name="phone"
           type="tel"
           autoComplete="phone"
           disabled={isSubmitting}
+          error={errors.phone?.message}
+          {...register('phone')}
         />
-        <div className="col-span-full">
-          <Button
-            type="submit"
-            variant="solid"
-            color="blue"
-            className="w-full"
-            disabled={isSubmitting}
-          >
-            <span>Submit</span>
-          </Button>
-        </div>
+        <Button
+          className="w-full col-span-full"
+          type="submit"
+          variant="solid"
+          color="blue"
+          disabled={isSubmitting}
+        >
+          <span>Submit</span>
+        </Button>
         <p className="col-span-full text-sm/6 text-gray-400">
           By submitting this form, you acknowledge that you’ve read and agree to
           our{' '}
