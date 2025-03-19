44e8625cf7bc833e6333aa0388f4fc338eff6515
Logan Kimball <logankimbs@gmail.com>
2025-03-19 12:48:17 -0700

    Added type to submitScheduleDemoForm data param

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,7 @@
-export async function submitScheduleDemoForm(data: any) {
-  const res = await fetch('api/demo', {
+import { ScheduleDemoFormFields } from '@/components/ScheduleDemoForm';
+
+export async function submitScheduleDemoForm(data: ScheduleDemoFormFields) {
+  const res = await fetch('api/schedule-demo', {
     method: 'POST',
     body: JSON.stringify(data),
   });
