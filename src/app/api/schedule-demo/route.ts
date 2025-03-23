a95c204913b52ddab7f2b4490640df053c0fc01b
Logan Kimball <logankimbs@gmail.com>
2025-03-23 09:46:55 -0700

    Update format

--- src/app/api/schedule-demo/route.ts
+++ src/app/api/schedule-demo/route.ts
@@ -24,6 +24,7 @@ export async function POST(req: Request) {
 
     await postScheduleDemoData(token, result.data);
     await clearSessionToken(token);
+
     return Response.json(null, { status: 200 });
   } catch (error: any) {
     console.error(error);
